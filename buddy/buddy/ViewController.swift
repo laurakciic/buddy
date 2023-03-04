2eb512887c2068a0137bb8cdc3ab7ae0e282d2c2
Laura <laurakovacic@outlook.com>
2023-03-04 13:16:34 +0100

    remove storyboard conf

---
+++ buddy/buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buddy
+//
+//  Created by Laura on 04.03.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
